b0af45d2354c53ef2d669193e909392ca318da26
NRowen1 <rowenrosario@gmail.com>
2026-03-04 01:18:36 -0500

    Delete db.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-//conect to local database
-    $conn = mysqli_connect("localhost","TheBeast","WeLoveCOP4331","COP4331"); //variable
-    if ($conn->connect_error) //check if connection was succesful
-    {
-        die(json_encode(["error"=> $conn->connect_error])); //stops execution and returns an error message if no conection was stablished (json file)
-    }
-?>
